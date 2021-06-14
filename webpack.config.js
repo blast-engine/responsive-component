5300691213bd3a5d60cfc773e50540fcc06ab094
santiago elustondo <elus2012@gmail.com>
2021-06-14 04:36:28 -0400

    auto

--- webpack.config.js
+++ webpack.config.js
@@ -41,10 +41,7 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['react-app']
-          }
+          loader: 'babel-loader'
         }
       }
     ]
